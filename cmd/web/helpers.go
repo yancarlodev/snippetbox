fa2b9427d4b052a305c03f270663ff49bc1e81a0
Yan Lepri <20251CAS10010011@estudantes.ifpr.edu.br>
2025-10-25 11:21:32 -0300

    feat: includes trace on server error log.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
-	method := r.Method
-	uri := r.URL.RequestURI()
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
+	)
 
-	app.logger.Error(err.Error(), slog.String("method", method), slog.String("uri", uri))
+	app.logger.Error(
+		err.Error(),
+		slog.String("method", method),
+		slog.String("uri", uri),
+		slog.String("trace", trace),
+	)
 
 	httpError := http.StatusInternalServerError
 	http.Error(w, http.StatusText(httpError), httpError)
